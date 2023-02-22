4534f1289657a465f2c71531f3ae69a41da235f7
PeterCullenBurbery <petercullenburbery@gmail.com>
2023-02-22 18:44:55 -0500

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -9,11 +9,13 @@
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
 select avg(salary) as "average salary for the company" from "public"."salaries"
-63810.744836143706
+-- $63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+select min(birth_date) from "public"."employees";
+1952-02-01
+-- 1952
 
 -- BEFORE YOU START
 /*
@@ -24,8 +26,8 @@ select avg(salary) as "average salary for the company" from "public"."salaries"
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
-
+--select count(*) from "public"."towns"
+--36684
 -- BEFORE YOU START
 /*
  * What database should I use for these exercises?
